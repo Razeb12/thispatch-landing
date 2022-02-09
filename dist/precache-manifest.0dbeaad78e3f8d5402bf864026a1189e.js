06915cc3bbf13de1f0c5b4c72fb0430671fb1721
Razeb Enyi <enyirazeb@gmail.com>
2022-02-09 17:58:30 +0100

    Update

---
+++ dist/precache-manifest.0dbeaad78e3f8d5402bf864026a1189e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c6f384dc5c9185586c",
+    "url": "/css/app.28a4ddfc.css"
+  },
+  {
+    "revision": "6f9ea52f3243ba1d090550688d244a16",
+    "url": "/img/image-10.6f9ea52f.png"
+  },
+  {
+    "revision": "91a6b2be0aeaa6b6f5beacdc6fb22b9b",
+    "url": "/img/logo.91a6b2be.png"
+  },
+  {
+    "revision": "897fc9a36abfd46dfabc7702513fca5c",
+    "url": "/img/mobile-app.897fc9a3.webp"
+  },
+  {
+    "revision": "0a4db7704f417c4593cf28c64971e98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae064df1d63a70597a7",
+    "url": "/js/about.6e3bb6c3.js"
+  },
+  {
+    "revision": "28c6f384dc5c9185586c",
+    "url": "/js/app.1b5c6736.js"
+  },
+  {
+    "revision": "7c8ee9e30fc3df81e6a9",
+    "url": "/js/chunk-vendors.faafc19e.js"
+  },
+  {
+    "revision": "755217e36a598c315afc3af2fa1330cd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
